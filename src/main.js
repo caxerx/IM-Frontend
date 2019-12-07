b5ef25bd5b8e6abdc1b9414b684546cc9c5ca22b
Caxer Tsang <caxer.tsang@atg.ai>
2019-12-08 03:58:45 +0800

    remove fonts import

--- src/main.js
+++ src/main.js
@@ -7,8 +7,6 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "@mdi/font/css/materialdesignicons.css";
 import CryptoJS from "crypto-js";
 window.cjs = CryptoJS;
 Vue.config.productionTip = false;
